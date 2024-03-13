edd0438ffa8b57872b416cd1c88d1ff922f2ee1e
leafney <babycoolzx@126.com>
2024-03-14 01:23:30 +0800

    feat: add putex and ttl

--- leveldb.go
+++ leveldb.go
@@ -1,9 +1,13 @@
 package rleveldb
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/syndtr/goleveldb/leveldb"
+	ldberrors "github.com/syndtr/goleveldb/leveldb/errors"
 	"github.com/syndtr/goleveldb/leveldb/filter"
 	"github.com/syndtr/goleveldb/leveldb/opt"
+	"time"
 )
 
 type LevelDB struct {
@@ -77,3 +81,106 @@ func (l *LevelDB) Begin() (*leveldb.Transaction, error) {
 //	iter := l.db.NewIterator(&util.Range{Start: start, Limit: end}, nil)
 //	return riterator{iter}
 //}
+
+// *******************
+
+type CacheType struct {
+	Data    []byte `json:"data"`
+	Created int64  `json:"created"`
+	Expire  int64  `json:"expire"`
+}
+
+func (l *LevelDB) PutEx(key []byte, value []byte, expires int64) error {
+	cache := CacheType{
+		Data:    value,
+		Created: time.Now().Unix(),
+		Expire:  0,
+	}
+
+	if expires > 0 {
+		cache.Expire = cache.Created + expires
+	}
+
+	jsonStr, err := json.Marshal(cache)
+	if err != nil {
+		return err
+	}
+
+	return l.db.Put(key, jsonStr, nil)
+}
+
+func (l *LevelDB) PutExString(key string, value string, expires int64) error {
+	return l.PutEx([]byte(key), []byte(value), expires)
+}
+
+func (l *LevelDB) GetEx(key []byte) ([]byte, error) {
+
+	data, err := l.db.Get(key, nil)
+	if err != nil && !errors.Is(err, ldberrors.ErrNotFound) {
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		return nil, nil
+	}
+
+	var cache CacheType
+	err = json.Unmarshal(data, &cache)
+	if err != nil {
+		return nil, err
+	}
+
+	secs := time.Now().Unix()
+
+	if cache.Expire > 0 && cache.Expire <= secs {
+		err = l.Delete(key)
+		return nil, err
+	}
+
+	return cache.Data, nil
+}
+
+func (l *LevelDB) GetExString(key string) (string, error) {
+	value, err := l.GetEx([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	if value == nil {
+		return "", nil
+	}
+
+	return string(value), nil
+}
+
+func (l *LevelDB) TTL(key []byte) (int64, error) {
+	data, err := l.db.Get(key, nil)
+	if err != nil && !errors.Is(err, ldberrors.ErrNotFound) {
+		return -1, err
+	}
+
+	if len(data) == 0 {
+		return -1, nil
+	}
+
+	var cache CacheType
+	err = json.Unmarshal(data, &cache)
+	if err != nil {
+		return -1, err
+	}
+
+	secs := time.Now().Unix()
+	if cache.Expire > 0 {
+		if cache.Expire <= secs {
+			return secs - cache.Expire, nil
+		} else {
+			return cache.Expire - secs, nil
+		}
+	}
+
+	return -1, nil
+}
+
+func (l *LevelDB) TTLString(key string) (int64, error) {
+	return l.TTL([]byte(key))
+}
--- leveldb_test.go
+++ leveldb_test.go
@@ -3,6 +3,7 @@ package rleveldb
 import (
 	"os"
 	"testing"
+	"time"
 )
 
 func TestNewLevelDB(t *testing.T) {
@@ -13,16 +14,30 @@ func TestNewLevelDB(t *testing.T) {
 	defer db.Close()
 
 	// 事务
-	tx, err := db.Begin()
-	if err != nil {
-		t.Log(err)
-		os.Exit(1)
-	}
+	/*
+		tx, err := db.Begin()
+		if err != nil {
+			t.Log(err)
+			os.Exit(1)
+		}
 
-	defer tx.Discard()
+		defer tx.Discard()
 
-	tx.Put([]byte(""), []byte(""), nil)
+		tx.Put([]byte(""), []byte(""), nil)
 
-	tx.Commit()
+		tx.Commit()
+
+	*/
+
+	//	PutEX
+
+	db.PutExString("hello", "world", 60)
+	time.Sleep(10 * time.Second)
+	t.Log(db.GetExString("hello"))
+	time.Sleep(20 * time.Second)
+	t.Log(db.TTLString("hello"))
+	time.Sleep(31 * time.Second)
+	t.Log(db.GetEx([]byte("hello")))
+	t.Log(db.TTL([]byte("hello")))
 
 }
